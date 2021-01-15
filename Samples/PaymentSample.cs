e8a075fc683261d76692ca30a85d9b030574056f
Burak Baldırlıoğlu <burak.baldirlioglu@craftbase.io>
2021-01-15 09:26:16 +0300

    fixes init 3ds deposit payment sample

--- Samples/PaymentSample.cs
+++ Samples/PaymentSample.cs
@@ -346,7 +346,8 @@ namespace Samples
             {
                 Price = new decimal(100.0),
                 PaidPrice = new decimal(100.0),
-                WalletPrice = new decimal(0.0),
+                WalletPrice = new decimal(100.0),
+                BuyerMemberId = 1,
                 ConversationId = "456d1297-908e-4bd6-a13b-4be31a6e47d5",
                 Currency = Currency.Try,
                 CallbackUrl = "https://www.your-website.com/craftgate-3DSecure-callback",
