bc21cc6eeb1dd63471bc35a1717ad75d78d7519f
Onur Polattimur <onur.polattimur@gmail.com>
2021-03-05 16:06:17 +0300

    update cpp callback url value

--- Samples/PaymentSample.cs
+++ Samples/PaymentSample.cs
@@ -510,7 +510,7 @@ namespace Samples
                 ConversationId = "456d1297-908e-4bd6-a13b-4be31a6e47d5",
                 Currency = Currency.Try,
                 PaymentGroup = PaymentGroup.ListingOrSubscription,
-                CallbackUrl = "https://www.your-website.com/craftgate-3DSecure-callback",
+                CallbackUrl = "https://www.your-website.com/craftgate-checkout-callback",
                 Items = new List<PaymentItem>
                 {
                     new PaymentItem
