65d92d859871a3ae3334ec0f2ca98e69574d087b
Onur Polattimur <onur.polattimur@gmail.com>
2021-03-05 16:10:28 +0300

    remove installment  field from init checkout payment request

--- Craftgate/Request/InitCheckoutPaymentRequest.cs
+++ Craftgate/Request/InitCheckoutPaymentRequest.cs
@@ -10,7 +10,6 @@ namespace Craftgate.Request
         public decimal? PaidPrice { get; set; }
         public string CallbackUrl { get; set; }
         public string CardUserKey { get; set; }
-        public int? Installment { get; set; }
         public Currency? Currency { get; set; }
         public PaymentGroup? PaymentGroup { get; set; }
         public string ConversationId { get; set; }
