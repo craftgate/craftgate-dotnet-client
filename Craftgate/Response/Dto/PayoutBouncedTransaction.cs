cc8250bd31e0bdc2157852c1cced872382e6f089
Sabri Onur Tüzün <335067+sotuzun@users.noreply.github.com>
2021-06-18 11:48:42 +0300

    adds payout fields to bounced rows resource (#32)

--- Craftgate/Response/Dto/PayoutBouncedTransaction.cs
+++ Craftgate/Response/Dto/PayoutBouncedTransaction.cs
@@ -1,11 +1,18 @@
+using System;
+
 namespace Craftgate.Response.Dto
 {
     public class PayoutBouncedTransaction
     {
         public long Id;
         public string Iban;
+        public DateTime CreatedDate;
+        public DateTime UpdatedDate;
+        public long PayoutId;
+        public decimal PayoutAmount;
         public string ContactName;
         public string ContactSurname;
         public string LegalCompanyTitle;
+        public string RowDescription;
     }
 }
