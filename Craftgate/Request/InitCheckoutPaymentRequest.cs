444ec718a48cf1d46220a1cdd0f11c21de334d84
Onur Polattimur <onur.polattimur@gmail.com>
2021-06-28 17:07:51 +0300

    add externalId field to InitCheckoutPaymentRequest

--- Craftgate/Request/InitCheckoutPaymentRequest.cs
+++ Craftgate/Request/InitCheckoutPaymentRequest.cs
@@ -13,6 +13,7 @@ namespace Craftgate.Request
         public Currency? Currency { get; set; }
         public PaymentGroup? PaymentGroup { get; set; }
         public string ConversationId { get; set; }
+        public string ExternalId { get; set; }
         public PaymentPhase PaymentPhase { get; set; } = PaymentPhase.Auth;
         public long? BuyerMemberId { get; set; }
         public string ClientIp { get; set; }
