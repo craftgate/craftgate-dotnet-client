8b5dff51643ea2b1d5bdb735e54abfa728f6f704
Burak BALDIRLIOGLU <burak.baldirlioglu@craftgate.io>
2021-10-20 12:14:19 +0300

    adds paymentChannel to payment request/response and search request (#49)

--- Craftgate/Request/InitCheckoutPaymentRequest.cs
+++ Craftgate/Request/InitCheckoutPaymentRequest.cs
@@ -13,6 +13,7 @@ namespace Craftgate.Request
         public string CardUserKey { get; set; }
         public Currency? Currency { get; set; }
         public PaymentGroup? PaymentGroup { get; set; }
+        public string PaymentChannel { get; set; }
         public string ConversationId { get; set; }
         public string ExternalId { get; set; }
         public PaymentPhase PaymentPhase { get; set; } = PaymentPhase.AUTH;
