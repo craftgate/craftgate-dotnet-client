92ecc3e885a937aba75ad65fefee2022041f86c4
Onur Polattimur <onur.polattimur@gmail.com>
2021-12-08 09:59:08 +0300

    Add YKB loyalty types (#59)

--- Craftgate/Model/LoyaltyType.cs
+++ Craftgate/Model/LoyaltyType.cs
@@ -5,5 +5,10 @@ namespace Craftgate.Model
     public enum LoyaltyType
     {
         [EnumMember(Value = "REWARD_MONEY")] REWARD_MONEY,
+        [EnumMember(Value = "ADDITIONAL_INSTALLMENT")] ADDITIONAL_INSTALLMENT,
+        [EnumMember(Value = "POSTPONING_INSTALLMENT")] POSTPONING_INSTALLMENT,
+        [EnumMember(Value = "EXTRA_POINTS")] EXTRA_POINTS,
+        [EnumMember(Value = "GAINING_MINUTES")] GAINING_MINUTES,
+        [EnumMember(Value = "POSTPONING_STATEMENT")] POSTPONING_STATEMENT
     }
 }
