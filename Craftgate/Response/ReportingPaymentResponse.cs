6773fb8d68ad6f840aec3aa78db8993121cdfe60
Sabri Onur Tüzün <335067+sotuzun@users.noreply.github.com>
2022-02-17 21:30:48 +0300

    adds transaction refund response (#68)

--- Craftgate/Response/ReportingPaymentResponse.cs
+++ Craftgate/Response/ReportingPaymentResponse.cs
@@ -12,5 +12,6 @@ namespace Craftgate.Response
         public int MdStatus { get; set; }
         public MemberResponse BuyerMember { get; set; }
         public IList<ReportingPaymentRefundResponse> Refunds { get; set; }
+        public IList<ReportingPaymentTransactionRefundResponse> PaymentTransactionRefunds { get; set; }
     }
 }
