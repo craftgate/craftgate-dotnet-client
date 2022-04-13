1835dc86fc74244494a745952da982fb38d72d1f
Alican Akkus <aakkus@Alicans-MacBook-Pro-2.local>
2022-04-13 16:15:40 +0300

    adds paybylink adapter

--- Craftgate/Adapter/PayByLinkApiAdapter.cs
+++ Craftgate/Adapter/PayByLinkApiAdapter.cs
@@ -45,36 +45,5 @@ namespace Craftgate.Adapter
             return RestClient.Get<ProductListResponse>(RequestOptions.BaseUrl + path,
                 CreateHeaders(path, RequestOptions));
         }
-
-        public MemberResponse CreateMember(CreateMemberRequest createMemberRequest)
-        {
-            var path = "/onboarding/v1/members";
-            return RestClient.Post<MemberResponse>(RequestOptions.BaseUrl + path,
-                CreateHeaders(createMemberRequest, path, RequestOptions),
-                createMemberRequest);
-        }
-
-        public MemberResponse UpdateMember(long id, UpdateMemberRequest updateMemberRequest)
-        {
-            var path = "/onboarding/v1/members/" + id;
-            return RestClient.Put<MemberResponse>(RequestOptions.BaseUrl + path,
-                CreateHeaders(updateMemberRequest, path, RequestOptions),
-                updateMemberRequest);
-        }
-
-        public MemberResponse RetrieveMember(long id)
-        {
-            var path = "/onboarding/v1/members/" + id;
-            return RestClient.Get<MemberResponse>(RequestOptions.BaseUrl + path,
-                CreateHeaders(path, RequestOptions));
-        }
-
-        public MemberListResponse SearchMembers(SearchMembersRequest searchMembersRequest)
-        {
-            var queryParam = RequestQueryParamsBuilder.BuildQueryParam(searchMembersRequest);
-            var path = "/onboarding/v1/members" + queryParam;
-            return RestClient.Get<MemberListResponse>(RequestOptions.BaseUrl + path,
-                CreateHeaders(path, RequestOptions));
-        }
     }
 }
