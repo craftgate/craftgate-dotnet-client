e861ff252be2e09a8d5b8dc8eab00f3bf70a1655
Alican Akkus <aakkus@Alicans-MacBook-Pro-2.local>
2022-04-14 15:38:24 +0300

    adds stock

--- Craftgate/Request/CreateProductRequest.cs
+++ Craftgate/Request/CreateProductRequest.cs
@@ -8,6 +8,7 @@ namespace Craftgate.Request
         public string Name {get; set;}
         public string Channel {get; set;}
         public decimal Price {get; set;}
+        public int Stock {get; set;}
         public Currency Currency {get; set;}
         public string Description {get; set;}
         public ISet<long> EnabledInstallments {get; set;}
