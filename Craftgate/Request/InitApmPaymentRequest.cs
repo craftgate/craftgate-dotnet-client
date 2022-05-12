c28d52fe0e0ef9d761c3d310f28b857513066031
Burak Baldırlıoğlu <burak.baldirlioglu@craftbase.io>
2022-05-12 09:51:47 +0300

    changes optional values for InitApmPaymentRequest

--- Craftgate/Request/InitApmPaymentRequest.cs
+++ Craftgate/Request/InitApmPaymentRequest.cs
@@ -7,9 +7,9 @@ namespace Craftgate.Request
     public class InitApmPaymentRequest
     {
         public ApmType ApmType { get; set; }
-        public decimal? Price { get; set; }
-        public decimal? PaidPrice { get; set; }
-        public Currency? Currency { get; set; }
+        public decimal Price { get; set; }
+        public decimal PaidPrice { get; set; }
+        public Currency Currency { get; set; }
         public PaymentGroup PaymentGroup { get; set; }
         public string PaymentChannel { get; set; }
         public string ConversationId { get; set; }
