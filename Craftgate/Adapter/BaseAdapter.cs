f6788034eac7615eb01c2b57201b339b86542914
Sabri Onur Tüzün <onur@craftgate.io>
2022-08-22 15:44:17 +0300

    prepares for next release v1.0.30

--- Craftgate/Adapter/BaseAdapter.cs
+++ Craftgate/Adapter/BaseAdapter.cs
@@ -43,7 +43,7 @@ namespace Craftgate.Adapter
             headers.Add(ApiKeyHeaderName, options.ApiKey);
             headers.Add(RandomHeaderName, randomString);
             headers.Add(AuthVersionHeaderName, ApiVersionHeaderValue);
-            headers.Add(ClientVersionHeaderName, ClientVersionHeaderValue + ":1.0.29");
+            headers.Add(ClientVersionHeaderName, ClientVersionHeaderValue + ":1.0.30");
             headers.Add(SignatureHeaderName, PrepareAuthorizationString(request, path, randomString, options));
             return headers;
         }
