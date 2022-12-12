feec081864f4dcb7499622a20379b9614ccbf1dc
Avni Balıkçı <avni.balikci@craftgate.io>
2022-12-12 17:36:18 +0300

    adds afterpay client sample

--- Samples/PaymentSample.cs
+++ Samples/PaymentSample.cs
@@ -1065,6 +1065,44 @@ namespace Samples
         }
         
         [Test]
+        public void Init_Afterpay_APM_Payment()
+        {
+            var request = new InitApmPaymentRequest
+            {
+                ApmType = ApmType.AFTERPAY,
+                Price = new decimal(1.0),
+                PaidPrice = new decimal(1.0),
+                Currency = Currency.USD,
+                PaymentGroup = PaymentGroup.LISTING_OR_SUBSCRIPTION,
+                ConversationId = "456d1297-908e-4bd6-a13b-4be31a6e47d5",
+                CallbackUrl = "https://www.your-website.com/craftgate-apm-callback",
+                Items = new List<PaymentItem>
+                {
+                    new PaymentItem
+                    {
+                        Name = "Item 1",
+                        ExternalId = Guid.NewGuid().ToString(),
+                        Price = new decimal(0.4)
+                    },
+
+                    new PaymentItem
+                    {
+                        Name = "Item 2",
+                        ExternalId = Guid.NewGuid().ToString(),
+                        Price = new decimal(0.6)
+                    }
+                }
+            };
+
+            var response = _craftgateClient.Payment().InitApmPayment(request);
+            Assert.NotNull(response);
+            Assert.NotNull(response.PaymentId);
+            Assert.NotNull(response.RedirectUrl);
+            Assert.AreEqual(response.PaymentStatus, PaymentStatus.WAITING);
+            Assert.AreEqual(response.AdditionalAction, ApmAdditionalAction.REDIRECT_TO_URL);
+        }
+        
+        [Test]
         public void Create_Cash_on_Delivery_Payment()
         {
             var request = new CreateApmPaymentRequest
