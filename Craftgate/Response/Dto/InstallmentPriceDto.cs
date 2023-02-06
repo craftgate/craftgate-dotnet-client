b8ebae622baa833171d83e7fb75cd81b3d1075e3
Alican Akkuş <alican.akkus@craftgate.io>
2023-02-06 11:07:30 +0300

    adds bank commission and merchant commission rates to installment response (#104)

--- Craftgate/Response/Dto/InstallmentPriceDto.cs
+++ Craftgate/Response/Dto/InstallmentPriceDto.cs
@@ -5,6 +5,8 @@ namespace Craftgate.Response.Dto
         public string PosAlias { get; set; }
         public int InstallmentNumber { get; set; }
         public decimal InstallmentPrice { get; set; }
+        public decimal BankCommissionRate { get; set; }
+        public decimal? MerchantCommissionRate { get; set; }
         public decimal TotalPrice { get; set; }
         public string InstallmentLabel { get; set; }
         public bool LoyaltySupported { get; set; }
