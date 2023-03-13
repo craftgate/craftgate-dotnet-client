c502c88bf1ef8e5e59022c1753b1445a0fcc257a
Burak Baldırlıoğlu <burak.baldirlioglu@craftbase.io>
2023-03-13 08:28:28 +0300

    renames fraud parameters

--- Craftgate/Request/Dto/FraudCheckParameters.cs
+++ Craftgate/Request/Dto/FraudCheckParameters.cs
@@ -3,7 +3,7 @@ namespace Craftgate.Request.Dto
     public class FraudCheckParameters
     {
         public string BuyerExternalId { get; set; }
-        public string BuyerGsmNumber { get; set; }
+        public string BuyerPhoneNumber { get; set; }
         public string BuyerEmail { get; set; }
     }
 }
