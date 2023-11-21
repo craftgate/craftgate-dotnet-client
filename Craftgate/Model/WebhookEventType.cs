eb5a411ddfdc0b586816c81f5b73f0df6cfe0841
Burak Baldırlıoğlu <burak.baldirlioglu@craftgate.io>
2023-11-21 17:33:49 +0300

    adds new event types

--- Craftgate/Model/WebhookEventType.cs
+++ Craftgate/Model/WebhookEventType.cs
@@ -7,6 +7,13 @@ namespace Craftgate.Model
         [EnumMember(Value = "API_AUTH")] API_AUTH,
         [EnumMember(Value = "API_VERIFY_AND_AUTH")] API_VERIFY_AND_AUTH,
         [EnumMember(Value = "CHECKOUTFORM_AUTH")] CHECKOUTFORM_AUTH,
-        [EnumMember(Value = "THREEDS_VERIFY")] THREEDS_VERIFY
+        [EnumMember(Value = "THREEDS_VERIFY")] THREEDS_VERIFY,
+        [EnumMember(Value = "REFUND")] REFUND,
+        [EnumMember(Value = "REFUND_TX")] REFUND_TX,
+        [EnumMember(Value = "PAYOUT_COMPLETED")] PAYOUT_COMPLETED,
+        [EnumMember(Value = "AUTOPILOT")] AUTOPILOT,
+        [EnumMember(Value = "WALLET_CREATED")] WALLET_CREATED,
+        [EnumMember(Value = "WALLET_TX_CREATED")] WALLET_TX_CREATED,
+        [EnumMember(Value = "BNPL_NOTIFICATION")] BNPL_NOTIFICATION
     }
 }
