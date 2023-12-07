f66f5137709b49ddbc70c7283167527b0259840b
Emre INAL <9061788+reywyn@users.noreply.github.com>
2023-12-07 09:48:20 +0300

    adds paymentStatus and additionalAction to initThreeDSPaymentResponse (#146)
    * adds paymentStatus and additionalAction to initThreeDSPaymentResponse
    
    * adds paymentStatus and additionalAction to initThreeDSPaymentResponse

--- Craftgate/Response/InitThreeDSPaymentResponse.cs
+++ Craftgate/Response/InitThreeDSPaymentResponse.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Text;
+using Craftgate.Model;
+using Craftgate.Response.Dto;
 
 namespace Craftgate.Response
 {
@@ -7,6 +9,8 @@ namespace Craftgate.Response
     {
         public string HtmlContent { get; set; }
         public long PaymentId { get; set; }
+        public PaymentStatus PaymentStatus { get; set; }
+        public AdditionalAction AdditionalAction { get; set; }
 
         public string GetDecodedHtmlContent()
         {
