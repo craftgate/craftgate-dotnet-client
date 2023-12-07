ade1cfbdbf79f3311d8cf33e5adada592ef32427
Emre INAL <emre.inal@craftgate.io>
2023-12-07 09:21:16 +0300

    adds paymentStatus and additionalAction to initThreeDSPaymentResponse

--- Craftgate/Response/InitThreeDSPaymentResponse.cs
+++ Craftgate/Response/InitThreeDSPaymentResponse.cs
@@ -7,6 +7,8 @@ namespace Craftgate.Response
     {
         public string HtmlContent { get; set; }
         public long PaymentId { get; set; }
+        public PaymentStatus PaymentStatus { get; set; }
+        public AdditionalAction AdditionalAction { get; set; }
 
         public string GetDecodedHtmlContent()
         {
