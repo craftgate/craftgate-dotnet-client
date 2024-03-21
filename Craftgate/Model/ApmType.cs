e3bd2e2726b300199488ec63bbd4c162a4ea2654
Alican Akkuş <alican.akkus@craftgate.io>
2024-03-21 16:03:42 +0300

    adds paycell as a new apm and provider type (#147)

--- Craftgate/Model/ApmType.cs
+++ Craftgate/Model/ApmType.cs
@@ -21,6 +21,7 @@ namespace Craftgate.Model
         [EnumMember(Value = "MASLAK")] MASLAK,
         [EnumMember(Value = "ALFABANK")] ALFABANK,
         [EnumMember(Value = "TOM_FINANCE")] TOM_FINANCE,
+        [EnumMember(Value = "PAYCELL")] PAYCELL,
         [EnumMember(Value = "HASO")] HASO,
         [EnumMember(Value = "FUND_TRANSFER")] FUND_TRANSFER,
         [EnumMember(Value = "CASH_ON_DELIVERY")] CASH_ON_DELIVERY
