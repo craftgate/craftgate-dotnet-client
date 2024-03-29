f09d1213a35897e82ae6e7e999b29ea8982515de
semihshn <s.semih.sahan@hotmail.com>
2024-03-29 16:10:02 +0300

    Update

--- Test/HashGeneratorTest.cs
+++ Test/HashGeneratorTest.cs
@@ -10,7 +10,7 @@ namespace Test
         public void Should_Generate_Hash()
         {
             //given
-            var expectedSignature = "lxoBkNdEanGAXYOL63uz/7P03kB0guaSPl7lXWvRJ4o=";
+            var expectedSignature = "9guTlRNDTRRyUsfSyJ8N/gAdNgQgUsL1guzqgG+FzN4=";
             var request = new CreateMemberRequest
             {
                 MemberExternalId = "ext-1511",
