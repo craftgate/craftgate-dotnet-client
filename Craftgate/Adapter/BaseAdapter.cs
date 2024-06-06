5628cf9ce626f6ee7a55559d2160832628951d29
Onur Polattimur <onur.polattimur@gmail.com>
2024-06-06 17:26:57 +0300

    Prepare for next release v1.0.61

--- Craftgate/Adapter/BaseAdapter.cs
+++ Craftgate/Adapter/BaseAdapter.cs
@@ -44,7 +44,7 @@ namespace Craftgate.Adapter
             headers.Add(ApiKeyHeaderName, options.ApiKey);
             headers.Add(RandomHeaderName, randomString);
             headers.Add(AuthVersionHeaderName, ApiVersionHeaderValue);
-            headers.Add(ClientVersionHeaderName, ClientVersionHeaderValue + ":1.0.60");
+            headers.Add(ClientVersionHeaderName, ClientVersionHeaderValue + ":1.0.61");
             headers.Add(SignatureHeaderName, PrepareAuthorizationString(request, path, randomString, options));
             if (options.Language != null)
             {
