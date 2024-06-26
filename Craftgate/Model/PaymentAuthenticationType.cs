2e270f766fe8ac3db3f6cf1878b414838ee1cfe4
Derya Cakmak <97deryacakmak@gmail.com>
2024-06-26 11:23:44 +0300

    BKM Express Integration

--- Craftgate/Model/PaymentAuthenticationType.cs
+++ Craftgate/Model/PaymentAuthenticationType.cs
@@ -9,5 +9,6 @@ namespace Craftgate.Model
     {
         [EnumMember(Value = "THREE_DS")] THREE_DS,
         [EnumMember(Value = "NON_THREE_DS")] NON_THREE_DS
+        [EnumMember(Value = "BKM_EXPRESS")] BKM_EXPRESS
     }
 }
