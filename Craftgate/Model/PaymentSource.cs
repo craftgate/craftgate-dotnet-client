d163848968a3ad8f91b4c66af9ff4ea52fdb2bd1
Derya Çakmak <36774966+deryacakmak@users.noreply.github.com>
2025-06-23 14:34:40 +0300

    Add cardBrandInstallments parameter (#240)

--- Craftgate/Model/PaymentSource.cs
+++ Craftgate/Model/PaymentSource.cs
@@ -7,6 +7,9 @@ namespace Craftgate.Model
         [EnumMember(Value = "API")] API,
         [EnumMember(Value = "CHECKOUT_FORM")] CHECKOUT_FORM,
         [EnumMember(Value = "PAY_BY_LINK")] PAY_BY_LINK,
-        [EnumMember(Value = "JUZDAN")] JUZDAN
+        [EnumMember(Value = "JUZDAN")] JUZDAN,
+        [EnumMember(Value = "BKM_EXPRESS")] BKM_EXPRESS,
+        [EnumMember(Value = "HEPSIPAY")] HEPSIPAY,
+        [EnumMember(Value = "MONO")] MONO,
     }
 }
