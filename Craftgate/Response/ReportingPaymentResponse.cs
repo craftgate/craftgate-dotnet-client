a39cdee236dfa49e34bdb50013b1804fe640a52f
Alican Akkus <aakkus@MacBook-Pro-2.local>
2025-07-21 10:14:24 +0300

    Add bank issuer id to response of card payment and loyalty retrieve

--- Craftgate/Response/ReportingPaymentResponse.cs
+++ Craftgate/Response/ReportingPaymentResponse.cs
@@ -8,7 +8,6 @@ namespace Craftgate.Response
         public int RetryCount { get; set; }
         public decimal RefundablePrice { get; set; }
         public string RefundStatus { get; set; }
-        public string CardIssuerBankName { get; set; }
         public MemberResponse BuyerMember { get; set; }
         public IList<ReportingPaymentRefundResponse> Refunds { get; set; }
     }
--- Craftgate/Response/RetrieveLoyaltiesResponse.cs
+++ Craftgate/Response/RetrieveLoyaltiesResponse.cs
@@ -7,6 +7,8 @@ namespace Craftgate.Response
     public class RetrieveLoyaltiesResponse
     {
         public string CardBrand { get; set; }
+        public string CardIssuerBankName;
+        public long CardIssuerBankId;
         public bool Force3ds { get; set; }
         public MerchantPos Pos { get; set; }
         public IList<Loyalty> Loyalties { get; set; }
