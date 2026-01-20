b903ef6fbfdaee5111697957e38135be3ae14bf3
furkanozmen <furkan.ozmen@craftgate.io>
2026-01-20 13:40:36 +0300

    Add secure fields token to StoreCardRequest and implement test for storing card

--- Craftgate/Request/StoreCardRequest.cs
+++ Craftgate/Request/StoreCardRequest.cs
@@ -6,6 +6,7 @@ namespace Craftgate.Request
         public string CardNumber { get; set; }
         public string ExpireYear { get; set; }
         public string ExpireMonth { get; set; }
+        public string SecureFieldsToken { get; set; }
         public string CardAlias { get; set; }
         public string CardUserKey { get; set; }
     }
