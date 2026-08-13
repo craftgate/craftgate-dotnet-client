f9c8aea7c8c202d526d96d8bf5f41bbfb6205a6e
Emre INAL <9061788+reywyn@users.noreply.github.com>
2026-08-13 12:43:28 +0300

    Prepare for next release v1.0.85 (#310)

--- Craftgate/Adapter/BaseAdapter.cs
+++ Craftgate/Adapter/BaseAdapter.cs
@@ -51,7 +51,7 @@ namespace Craftgate.Adapter
             headers.Add(ApiKeyHeaderName, requestOptions.ApiKey);
             headers.Add(RandomHeaderName, randomString);
             headers.Add(AuthVersionHeaderName, ApiVersionHeaderValue);
-            headers.Add(ClientVersionHeaderName, ClientVersionHeaderValue + ":1.0.83");
+            headers.Add(ClientVersionHeaderName, ClientVersionHeaderValue + ":1.0.85");
             headers.Add(SignatureHeaderName, PrepareAuthorizationString(request, path, randomString, requestOptions));
             if (requestOptions.Language != null)
             {
